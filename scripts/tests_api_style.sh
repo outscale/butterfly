752290b3705a24456fc3f62c034c72919179ad91
Jérôme Jutteau <jerome.jutteau@outscale.com>
2015-08-11 17:03:17 +0200

    scripts: update api style check
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- scripts/tests_api_style.sh
+++ scripts/tests_api_style.sh
@@ -25,8 +25,11 @@ $BUTTERFLY_ROOT/api/server/model.cc \
 $BUTTERFLY_ROOT/api/server/model.h \
 $BUTTERFLY_ROOT/api/server/api.cc \
 $BUTTERFLY_ROOT/api/server/api.h \
-$BUTTERFLY_ROOT/api/server/api_0.cc"
-
+$BUTTERFLY_ROOT/api/server/api_0.cc \
+$BUTTERFLY_ROOT/api/server/pg.cc \
+$BUTTERFLY_ROOT/api/server/pg.h \
+$BUTTERFLY_ROOT/api/server/graph.cc \
+$BUTTERFLY_ROOT/api/server/graph.h"
 
 $BUTTERFLY_ROOT/scripts/cpplint.py --filter=-build/c++11 --root=$BUTTERFLY_ROOT $sources
 if [ $? != 0 ]; then
