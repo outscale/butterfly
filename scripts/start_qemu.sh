a6204337882d978ca35ee7592ecd546469e011d4
Jérôme Jutteau <jerome.jutteau@outscale.com>
2015-10-08 16:01:17 +0200

    scripts: update start_qemu.sh to get MAC address in parameters
    Also add a nic to have an external network access
    
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- scripts/start_qemu.sh
+++ scripts/start_qemu.sh
@@ -7,8 +7,9 @@ fi
 
 SOCKET_PATH=$1
 IMP_PATH=$2
+MAC=$3
 
-CMD="qemu-system-x86_64 -m 124M -enable-kvm -chardev socket,id=char0,path=$SOCKET_PATH -netdev type=vhost-user,id=mynet1,chardev=char0,vhostforce -device virtio-net-pci,mac=52:54:00:12:34:11,netdev=mynet1 -object memory-backend-file,id=mem,size=124M,mem-path=/mnt/huge,share=on -numa node,memdev=mem -mem-prealloc  -drive file=$IMP_PATH --curses  "
+CMD="qemu-system-x86_64 -m 124M -enable-kvm -chardev socket,id=char0,path=$SOCKET_PATH -netdev type=vhost-user,id=mynet1,chardev=char0,vhostforce -device virtio-net-pci,mac=$MAC,netdev=mynet1 -object memory-backend-file,id=mem,size=124M,mem-path=/mnt/huge,share=on -numa node,memdev=mem -mem-prealloc  -drive file=$IMP_PATH --curses  "
 echo -n 'executing: '
 echo $CMD
 exec $CMD
