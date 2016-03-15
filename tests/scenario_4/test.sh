9a5e40945496bbcc0c1300f9f49f7b683a6d28eb
Jerome Jutteau <jerome.jutteau@outscale.com>
2016-03-15 15:35:15 +0100

    tests: add a stop/start scenario
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

---
+++ tests/scenario_4/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BUTTERFLY_SRC_ROOT=$1
+BUTTERFLY_BUILD_ROOT=$2
+
+source $BUTTERFLY_SRC_ROOT/tests/functions.sh
+
+network_connect 0 1
+server_start 0
+client_add_nic 0 1 42
+client_add_nic 0 2 42
+qemu_start 1
+qemu_start 2
+ssh_ping 1 2
+ssh_ping 2 1
+qemu_stop 1
+qemu_start 1
+ssh_ping 1 2
+ssh_ping 2 1
+qemu_stop 1
+qemu_start 1
+ssh_ping 1 2
+ssh_ping 2 1
+qemu_stop 1
+qemu_stop 2
+server_stop 0
+network_disconnect 0 1
+return_result
+
