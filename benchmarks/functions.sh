2db027ec4d14918f6b34bd0842033faba06f20ce
Jerome Jutteau <jerome.jutteau@outscale.com>
2016-06-21 17:17:56 +0200

    benchmarks: run speed and latency tests on remote machines
    closes #60
    
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

---
+++ benchmarks/functions.sh
@@ -0,0 +1,216 @@
+IMG_URL=https://osu.eu-west-2.outscale.com/jerome.jutteau/16d1bc0517de5c95aa076a0584b43af6/arch-051115.qcow
+IMG_MD5=4b7b1a71ac47eb73d85cdbdc85533b07
+KEY_URL=https://osu.eu-west-2.outscale.com/jerome.jutteau/16d1bc0517de5c95aa076a0584b43af6/arch-051115.rsa
+KEY_MD5=eb3d700f2ee166e0dbe00f4e0aa2cef9
+
+function ssh_run {
+    local ip=$1
+    local port=$2
+    local cmd="${@:3}"
+    ssh -p $port -l root $ip $cmd
+}
+
+function ping_test {
+    local ip=$1
+    if ! ping -c 1 $ip &> /dev/null; then
+        exit 1
+    fi
+}
+
+function remote_cp {
+    local ip=$1
+    local port=$2
+    local local_file=$3
+    local dist_file=$4
+    scp -P $port $local_file root@$ip:$dist_file
+}
+
+function clean_hard {
+    local ip=$1
+    local port=$2
+    local tmp=$3
+    ssh_run $ip_a $port_a pkill -h &> /dev/null
+    ssh_run $ip $port pkill --signal 9 -f butterfly-server || true
+    ssh_run $ip $port pkill --signal 9 -f butterfly-client || true
+    ssh_run $ip $port pkill --signal 9 -f qemu-system-x86_64 || true
+    ssh_run $ip $port rm -rf /tmp/*vhost* /dev/hugepages/* /mnt/huge/* $tmp || true
+    ssh_run $ip $port yum remove -y butterfly &> /dev/null || true
+    ssh_run $ip $port apt-get remove --purge -q butterfly &> /dev/null || true
+}
+
+function butterfly_start {
+    local id=$1
+    local ip=$2
+    local port=$3
+    local CMD="butterfly-server --no-shconf -c1 -n2 --socket-mem 64 -- -l debug -i 43.0.0.$id -s /tmp"
+    ssh_run $ip $port tmux new -d -s $(date +%Y-%m-%d-%H.%M.%S) \'$CMD\'
+    sleep 5
+    ssh_run $ip $port pgrep -f butterfly-server
+}
+
+function nic_add {
+    local ip=$1
+    local port=$2
+    local id=$3
+    local vni=$4
+    f=/tmp/butterfly-client.req
+
+    echo -e "messages {
+  revision: 0
+  message_0 {
+    request {
+      nic_add {
+        id: \"nic-$id\"
+        mac: \"52:54:00:12:34:0$id\"
+        vni: $vni
+        ip: \"42.0.0.$id\"
+        ip_anti_spoof: true
+        security_group: \"sg-1\"
+      }
+    }
+  }
+}
+messages {
+  revision: 0
+  message_0 {
+    request {
+      sg_add {
+        id: \"sg-1\"
+        rule {
+          direction: INBOUND
+          protocol: -1
+          cidr {
+            address: \"0.0.0.0\"
+            mask_size: 0
+          }
+        }
+      }
+    }
+  }
+}" > $f
+    remote_cp $ip $port $f $f
+    ssh_run $ip $port butterfly-client -e tcp://$ip:9999 -i $f
+}
+
+function ssh_vm {
+    local ip=$1
+    local port=$2
+    local id=$3
+    local cmd="${@:4}"
+    ssh -p $port -l root $ip ssh -p 600$id -l root -i /var/tmp/image.rsa -oStrictHostKeyChecking=no -oConnectTimeout=60 127.0.0.1 $cmd
+}
+
+function vm_start {
+    local ip=$1
+    local port=$2
+    local id=$3
+    SOCKET_PATH=/tmp/qemu-vhost-nic-$id
+    IMG_PATH=/var/tmp/image.qcow
+    KEY_PATH=/var/tmp/image.rsa
+    MAC=52:54:00:12:34:0$id
+
+    CMD="qemu-system-x86_64 -cpu host -smp 1 -netdev user,id=network0,hostfwd=tcp::600${id}-:22 -device e1000,netdev=network0 -m 256M -enable-kvm -chardev socket,id=char0,path=$SOCKET_PATH -netdev type=vhost-user,id=mynet1,chardev=char0,vhostforce -device virtio-net-pci,mac=$MAC,netdev=mynet1 -object memory-backend-file,id=mem,size=256M,mem-path=/mnt/huge,share=on -numa node,memdev=mem -mem-prealloc -drive file=$IMG_PATH -snapshot -nographic"
+    ssh_run $ip $port $CMD &
+
+    sleep 5
+    ssh_run $ip $port pgrep -f qemu-system-x86_64
+
+    # Wait for ssh to be ready
+    ssh_vm $ip $port $id true || ssh_vm $ip $port $id true || ssh_vm $ip $port $id true
+
+    # Configure IP on vhost interface
+    ssh_vm $ip $port $id ip link set ens4 up
+    ssh_vm $ip $port $id ip addr add 42.0.0.$id/16 dev ens4
+    #ssh_vm $ip $port $id ip link set dev ens4 mtu 1400
+}
+
+function icmp_bench {
+    local output=$1
+    local ip=$2
+    local port=$3
+    local from_id=$4
+    local to_id=$5
+    local same_host=$6
+    local tmp=/tmp/bench_output
+    local state="ok"
+    local min=""
+    local avg=""
+    local max=""
+    local geo=""
+    ssh_vm $ip $port $from_id ping 42.0.0.$to_id -c 1 || true
+    ssh_vm $ip $port $from_id ping 42.0.0.$to_id -c $bench_duration > $tmp || state="ko" 
+
+    if [ "$state" = "ok" ]; then
+        min=$(cat $tmp | tail -n 1 | cut -d ' ' -f 4 | cut -d '/' -f 1)
+        avg=$(cat $tmp | tail -n 1 | cut -d ' ' -f 4 | cut -d '/' -f 2)
+        max=$(cat $tmp | tail -n 1 | cut -d ' ' -f 4 | cut -d '/' -f 3)
+    fi
+    if ! [ ".$same_host" = "." ]; then
+        geo="same host"
+    else
+        geo="different host"
+    fi
+    echo "PING;$state;$geo;$from_id;$to_id;$min;$avg;$max" >> $output
+}
+
+function tcp_bench {
+    local output=$1
+    local ip_client=$2
+    local port_client=$3
+    local id_client=$4
+    local ip_server=$5
+    local port_server=$6
+    local id_server=$7
+    local tmp=/tmp/bench_output
+    local state="ok"
+    local r=""
+    local geo=""
+    local port=$((RANDOM % (65535 - 1025) + 1025))
+    ssh_vm $ip_server $port_server $id_server iperf3 -p $port -s &
+    local p=$!
+    sleep 1
+    ssh_vm $ip_client $port_client $id_client iperf3 -p $port -t $bench_duration -c 42.0.0.$id_server --zerocopy --no-delay --json > $tmp || true
+    kill $p
+    if [ ".$(cat $tmp)" = "." ]; then
+        state="ko"
+    else
+        r=$(cat $tmp | grep bits_per_second | tail -n 1 | cut -d ':' -f 2 | tr -d '\t')
+    fi
+    if [ $ip_client = $ip_server ] && [ $port_client = $port_server ]; then
+        geo="same host"
+    else
+        geo="different host"
+    fi
+    echo "TCP;$state;$geo;$id_client;$id_server;$r" >> $output
+}
+
+function udp_bench {
+    local output=$1
+    local ip_client=$2
+    local port_client=$3
+    local id_client=$4
+    local ip_server=$5
+    local port_server=$6
+    local id_server=$7
+    local tmp=/tmp/bench_output
+    local state="ok"
+    local r=""
+    local geo=""
+    local port=$((RANDOM % (65535 - 1025) + 1025))
+    ssh_vm $ip_server $port_server $id_server iperf3 -p $port -s &
+    local p=$!
+    sleep 1
+    ssh_vm $ip_client $port_client $id_client iperf3 -p $port -l 1400 -t $bench_duration -c 42.0.0.$id_server --zerocopy -u -b 10G --reverse --json > $tmp || true
+    sleep 1
+    if [ ".$(cat $tmp)" = "." ]; then
+        state="ko"
+    else
+        r=$(cat $tmp | grep bits_per_second | tail -n 1 | cut -d ':' -f 2 | tr -d '\t')
+    fi
+    if [ $ip_client = $ip_server ] && [ $port_client = $port_server ]; then
+        geo="same host"
+    else
+        geo="different host"
+    fi
+    echo "UDP;$state;$geo;$id_client;$id_server;$r" >> $output
+}
---
+++ benchmarks/run_benchmarks.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+set -e
+
+function usage {
+    echo "This script connects to two machines and launch several performance"
+    echo "tests between two machines based on locally build and packaged"
+    echo "Butterfly."
+    echo "We assume that machines:"
+    echo "- Already have their DPDK ports configured"
+    echo "- Have their dpdk test ports connected together"
+    echo "- Have Qemu installed"
+    echo "- Huge pages are configured"
+    echo "- SSH keys configured"
+    echo "It things goes wrong, run this script with bash -x"
+    echo ""
+    echo "Usage: run_benchmark.sh"
+    echo "    -s | --sources : Butterfly source directory (mandatory)"
+    echo "    -b | --build   : Butterfly build directoty (mandatory)"
+    echo "    -o | --output  : file where to write benchmark"
+    echo "                     (optional, default: output.csv)"
+    echo "    --ip-a         : IP address of first machine (mandatory)"
+    echo "    --port-a       : SSH port of first machine (optional, default: 22)"
+    echo "    --ip-b         : IP address of second machine (mandatory)"
+    echo "    --port-b       : SSH port of second machine (optional, default: 22)"
+    echo "    --package      : 'rpm' or 'deb' (optional, default: rpm)"
+    echo "    -t | --time    : duration (seconds) of each benchmark"
+    echo "                     (optional, default: 100)"
+    echo "    --keep-running : launch endless TCP iperf before leaving"
+    echo "    -h | --help    : show this help"
+}
+
+# read arguments
+
+source_dir=""
+build_dir=""
+run_dir=$(pwd)
+ip_a=""
+port_a=22
+ip_b=""
+port_b=22
+package='rpm'
+output="$(pwd)/output.csv"
+bench_duration=100
+keep_running=0
+
+args=`getopt -o s:b:o:t:h:: --long sources:,build:,output:,ip-a:,ip-b:,port-a:,port-b:,package:,time:,keep-running,help:: -- "$@"`
+eval set -- "$args"
+while true ; do
+    case "$1" in
+        -s|--sources)
+            source_dir=$2 ; shift 2 ;;
+        -b|--build)
+            build_dir=$2 ; shift 2 ;;
+        -o|--output)
+            output=$2 ; shift 2 ;;
+        --ip-a)
+            ip_a=$2 ; shift 2 ;;
+        --port-a)
+            port_a=$2 ; shift 2 ;;
+        --ip-b)
+            ip_b=$2 ; shift 2 ;;
+        --port-b)
+            port_b=$2 ; shift 2 ;;
+        --package)
+            package=$2 ; shift 2 ;;
+        -t|--time)
+            bench_duration=$2 ; shift 2 ;;
+        --keep-running)
+            keep_running=1 ; shift 1 ;;
+        -h|--help)
+            usage; exit 0 ;;
+        --)
+            break ;;
+        *)
+            usage; exit 1 ;;
+    esac
+done
+
+# check arguments
+
+if [ -z "$source_dir" ]; then
+    echo -e "Error: Butterfly source dir not set\n"
+    usage
+    exit 1
+fi
+
+if [ -z "$build_dir" ]; then
+    echo -e "Error: Butterfly build dir not set\n"
+    usage
+    exit 1
+fi
+
+if [ -z "$ip_a" ]; then
+    echo -e "Error: IP of machine A not set\n"
+    usage
+    exit 1
+fi
+
+if [ -z "$ip_b" ]; then
+    echo -e "Error: IP of machine B not set\n"
+    usage
+    exit 1
+fi
+
+f=$source_dir/CMakeLists.txt
+if [ ! -f $f ]; then
+    echo -e "Error: Butterfly source dir seems bad: $f does not exist\n"
+    usage
+    exit 1
+fi
+
+f=$build_dir/CMakeCache.txt
+if [ ! -f $f ]; then
+    echo -e "Error: Butterfly build dir seems bad: $f does not exist\n"
+    usage
+    exit 1
+fi
+
+case "$package" in
+    rpm) ;;
+    deb) ;;
+    *)
+        echo -e "Invalid package type $package\n"
+        usage
+        exit 1
+        ;;
+esac
+
+source $source_dir/benchmarks/functions.sh
+ping_test $ip_a
+ping_test $ip_b
+
+ssh_run $ip_a $port_a true
+ssh_run $ip_b $port_b true
+
+# build butterfly
+cd $build_dir
+rm *.$package || true
+make
+make package-$package
+
+# clean targets
+tmp=/root/tmp_butterfly
+clean_hard $ip_a $port_a $tmp
+clean_hard $ip_b $port_b $tmp
+(cd $run_dir && rm $output) || true
+
+# install butterfly
+ssh_run $ip_a $port_a mkdir $tmp
+ssh_run $ip_b $port_b mkdir $tmp
+
+remote_cp $ip_a $port_a *.$package $tmp/butterfly.$package
+remote_cp $ip_b $port_b *.$package $tmp/butterfly.$package
+
+ssh_run $ip_a $port_a rpm -i $tmp/butterfly.rpm || \
+ssh_run $ip_a $port_a dpkg -i $tmp/butterfly.deb
+ssh_run $ip_b $port_b rpm -i $tmp/butterfly.rpm || \
+ssh_run $ip_b $port_b dpkg -i $tmp/butterfly.deb
+
+# check that remote machines has needed binaries
+ssh_run $ip_a $port_a butterfly-server -h &> /dev/null
+ssh_run $ip_b $port_b butterfly-server -h &> /dev/null
+
+ssh_run $ip_a $port_a qemu-system-x86_64 -h &> /dev/null
+ssh_run $ip_b $port_b qemu-system-x86_64 -h &> /dev/null
+
+ssh_run $ip_a $port_a test -e /usr/bin/iperf &> /dev/null
+ssh_run $ip_b $port_b test -e /usr/bin/iperf &> /dev/null
+
+ssh_run $ip_a $port_a tmux new -d -s $(date +%Y-%m-%d-%H.%M.%S) true &> /dev/null
+ssh_run $ip_b $port_b tmux new -d -s $(date +%Y-%m-%d-%H.%M.%S) true &> /dev/null
+
+# launch butterfly
+butterfly_start 1 $ip_a $port_a
+butterfly_start 2 $ip_b $port_b
+sleep 5
+
+# create some network interface
+nic_add $ip_a $port_a 1 42
+nic_add $ip_a $port_a 2 42
+nic_add $ip_b $port_b 3 42
+
+# launch virtual machines
+declare -a pids
+vtmp=/var/tmp
+ssh_run $ip_a $port_a test -e $vtmp/image.qcow || ssh_run $ip_a $port_a wget -q $IMG_URL -O $vtmp/image.qcow &
+pids[$!]=$!
+ssh_run $ip_b $port_b test -e $vtmp/image.qcow || ssh_run $ip_b $port_b wget -q $IMG_URL -O $vtmp/image.qcow &
+pids[$!]=$!
+ssh_run $ip_a $port_a test -e $vtmp/image.rsa || ssh_run $ip_a $port_a wget -q $KEY_URL -O $vtmp/image.rsa &
+pids[$!]=$!
+ssh_run $ip_b $port_b test -e $vtmp/image.rsa || ssh_run $ip_b $port_b wget -q $KEY_URL -O $vtmp/image.rsa &
+pids[$!]=$!
+wait ${pids[*]};
+unset $pids;
+
+ssh_run $ip_a $port_a chmod 600 /var/tmp/image.rsa
+ssh_run $ip_b $port_b chmod 600 /var/tmp/image.rsa
+
+# 2 hosts A and B:
+# VM 1 is on host A
+# VM 2 is on host A
+# VM 3 is on host B
+declare -a pids;
+vm_start $ip_a $port_a 1 &
+pids[$!]=$!
+vm_start $ip_a $port_a 2 &
+pids[$!]=$!
+vm_start $ip_b $port_b 3 &
+pids[$!]=$!
+wait ${pids[*]};
+unset $pids;
+
+cd $run_dir
+
+# launch ICMP test between two machines on the same host
+icmp_bench $output $ip_a $port_a 1 2 same_host
+
+# launch ICMP test between two machines on different host
+icmp_bench $output $ip_a $port_a 1 3
+
+# launch TCP test between two machines on the same host
+tcp_bench $output $ip_a $port_a 1 $ip_a $port_a 2
+
+# launch TCP test between two machines on different host
+tcp_bench $output $ip_a $port_a 1 $ip_b $port_b 3
+
+# launch UDP test between two machines on the same host
+udp_bench $output $ip_a $port_a 1 $ip_a $port_a 2
+
+# launch UDP test between two machines on different host
+udp_bench $output $ip_a $port_a 1 $ip_b $port_b 3
+
+if [ $keep_running = "1" ]; then
+    ssh_vm $ip_b $port_b 3 iperf -s & &> /dev/null
+    sleep 1
+    ssh_vm $ip_a $port_a 1 iperf -t 0 -c 42.0.0.3
+fi
+
+echo "Benchmark end, check $output file:"
+cat $output
