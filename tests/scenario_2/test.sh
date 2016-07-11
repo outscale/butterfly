cd8318c1855a07f1390dc488fd6b3175a88cdffa
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-07-11 11:34:49 +0000

    tests: add functions for tcp/udp tests
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

--- tests/scenario_2/test.sh
+++ tests/scenario_2/test.sh
@@ -8,10 +8,10 @@ source $BUTTERFLY_SRC_ROOT/tests/functions.sh
 network_connect 0 1
 server_start 0
 server_start 1
-add_nic 0 1 42
-add_nic 0 2 42
-add_nic 1 3 42
-add_nic 1 4 42
+add_nic sg-1 0 1 42
+add_nic sg-1 0 2 42
+add_nic sg-1 1 3 42
+add_nic sg-1 1 4 42
 qemu_start 1
 qemu_start 2
 qemu_start 3
