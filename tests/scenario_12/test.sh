849b359d1b491dcbba25ec76b624c9a15fbe46de
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-07-11 17:19:07 +0000

    tests: add udp test on two butterflies
    closed #34
    
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

---
+++ tests/scenario_12/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BUTTERFLY_SRC_ROOT=$1
+BUTTERFLY_BUILD_ROOT=$2
+
+source $BUTTERFLY_SRC_ROOT/tests/functions.sh
+
+network_connect 0 1
+server_start 0
+server_start 1
+add_nic 0 1 42
+add_nic 0 2 1337
+add_nic 1 3 1337
+add_nic 1 4 42
+qemu_start 1
+qemu_start 2
+qemu_start 3
+qemu_start 4
+ssh_udp 1 4 5554
+ssh_udp 4 1 5554
+ssh_no_udp 1 2 5554
+ssh_no_udp 1 3 5034
+ssh_no_udp 4 2 5550
+ssh_no_udp 4 3 5554
+qemu_stop 1
+qemu_stop 2
+qemu_stop 3
+qemu_stop 4
+server_stop 0
+server_stop 1
+network_disconnect 0 1
+return_result
+
