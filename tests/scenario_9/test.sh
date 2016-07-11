59cb1279a5476d339007d0aed350608a66828132
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-07-11 16:16:48 +0000

    tests: add tcp test on one butterfly
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

---
+++ tests/scenario_9/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUTTERFLY_SRC_ROOT=$1
+BUTTERFLY_BUILD_ROOT=$2
+
+source $BUTTERFLY_SRC_ROOT/tests/functions.sh
+
+network_connect 0 1
+server_start 0
+add_nic sg-1 0 1 42 
+add_nic sg-1 0 2 42
+qemu_start 1
+qemu_start 2
+ssh_connection_test tcp 1 2 4045
+ssh_connection_test tcp 2 1 1445
+qemu_stop 1
+qemu_stop 2
+server_stop 0
+network_disconnect 0 1
+return_result
+
