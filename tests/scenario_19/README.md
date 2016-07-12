0a6a416d762f9dd791ac85ce5819eca3292c1c65
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-07-12 09:59:47 +0000

    tests: add udp/tcp test without sg rules
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

---
+++ tests/scenario_19/README.md
@@ -0,0 +1,16 @@
+# Description
+
+```
++-----------+
+|           |-----------[ VM 1 ] (vni 42)
+| Butterfly |
+|           |-----------[ VM 2 ] (vni 42)
++-----------+
+
+```
+
+This scenario just connects two virtual machines on the same butterfly
+on the same network (vni) without security group rules and make a bunch
+of network tests (tcp and udp). Update security group rules and make a
+bunch of network tests (tcp and udp).
+
---
+++ tests/scenario_19/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BUTTERFLY_SRC_ROOT=$1
+BUTTERFLY_BUILD_ROOT=$2
+
+source $BUTTERFLY_SRC_ROOT/tests/functions.sh
+
+network_connect 0 1
+server_start 0
+add_nic_no_rules sg-1 0 1 42
+add_nic_no_rules sg-1 0 2 42
+qemu_start 1
+qemu_start 2
+ssh_no_connection_test udp 2 1 5554
+ssh_no_connection_test udp 1 2 5554
+ssh_no_connection_test tcp 1 2 4445
+ssh_no_connection_test tcp 2 1 4445
+
+add_sg_rule_full_open sg-1 0 1
+ssh_connection_test udp 1 2 4445
+ssh_connection_test udp 2 1 5554
+ssh_connection_test tcp 2 1 5554
+ssh_connection_test tcp 1 2 3485
+qemu_stop 1
+qemu_stop 2
+server_stop 0
+network_disconnect 0 1
+return_result
+
