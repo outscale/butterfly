1ec7baf826a44b75c9d101fcc02a79911af4574d
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-09-01 14:48:58 +0000

    tests: rename tcp/udp test functions
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

--- tests/scenario_18/test.sh
+++ tests/scenario_18/test.sh
@@ -7,15 +7,15 @@ source $BUTTERFLY_SRC_ROOT/tests/functions.sh
 
 network_connect 0 1
 server_start 0
-add_nic_void 0 1 42
-add_nic sg-1 0 2 42
+nic_add_void 0 1 42
+nic_add sg-1 0 2 42
 qemu_start 1
 qemu_start 2
 ssh_connection_test udp 2 1 1243
 ssh_connection_test tcp 2 1 5543
 ssh_no_connection_test udp 1 2 1234
 ssh_no_connection_test tcp 1 2 1254
-add_sg sg-1 0 1
+set_nic_sg sg-1 0 1
 ssh_connection_test udp 2 1 1243
 ssh_connection_test tcp 2 1 5543
 ssh_connection_test udp 1 2 1234
@@ -27,7 +27,7 @@ ssh_no_connection_test udp 2 1 5043
 ssh_no_connection_test tcp 1 2 1043
 ssh_no_connection_test udp 1 2 5503
 
-add_sg_rule_full_open sg-1 0 1
+sg_rule_add_full_open sg-1 0 1
 ssh_connection_test udp 2 1 1243
 ssh_connection_test tcp 2 1 5543
 ssh_connection_test udp 1 2 1234
