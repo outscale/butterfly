ece1877ced9ae7bf591776b1f959a1c4ea9aa1c2
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-09-05 13:36:18 +0000

    tests: add/delete sg member in tcp/udp test
    closed #47
    
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

---
+++ tests/scenario_27/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BUTTERFLY_SRC_ROOT=$1
+BUTTERFLY_BUILD_ROOT=$2
+
+source $BUTTERFLY_SRC_ROOT/tests/functions.sh
+
+network_connect 0 1
+server_start 0
+nic_add_port_open udp sg-1 0 1 42 8000
+nic_add_port_open udp sg-2 0 2 42 9000
+qemu_start 1
+qemu_start 2
+
+ssh_connection_test udp 1 2 8000
+ssh_connection_test udp 2 1 9000
+ssh_no_connection_test tcp 1 2 9000
+ssh_no_connection_test tcp 2 1 8000
+
+sg_rule_add_with_sg_member udp sg-1 0 sg-2
+sg_rule_add_with_sg_member udp sg-2 0 sg-1
+ssh_connection_test udp 1 2 8000
+ssh_connection_test udp 1 2 9000
+ssh_connection_test udp 2 1 9000
+ssh_connection_test udp 2 1 8000
+
+sg_rule_del_with_sg_member udp sg-1 0 sg-2
+sg_rule_del_with_sg_member udp sg-2 0 sg-1
+ssh_connection_test udp 1 2 8000
+ssh_connection_test udp 2 1 9000
+ssh_no_connection_test tcp 1 2 9000
+ssh_no_connection_test tcp 2 1 8000
+qemu_stop 1
+qemu_stop 2
+server_stop 0
+network_disconnect 0 1
+return_result
