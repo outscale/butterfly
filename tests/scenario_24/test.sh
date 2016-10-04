a78a1a1f5a44ef601d00a72c67b6b12e8bfecb8d
Thiery Ouattara <thiery.ouattara@outscale.com>
2016-10-04 14:16:12 +0000

    tests: remove useless checking failed test
    Signed-off-by: Thiery Ouattara <thiery.ouattara@outscale.com>

--- tests/scenario_24/test.sh
+++ tests/scenario_24/test.sh
@@ -13,44 +13,12 @@ qemu_start 1
 qemu_start 2
 for i in {1..50}; do
     ssh_connection_test udp 1 2 1234
-    ret=$?
-    if [ $ret == 1 ]; then
-	qemu_stop 1
-	qemu_stop 2
-	server_stop 0
-	network_disconnect 0 1
-	return_result
-    fi
     ssh_connection_test udp 2 1 1234
-    ret=$?
-    if [ $ret == 1 ]; then
-	qemu_stop 1
-	qemu_stop 2
-	server_stop 0
-	network_disconnect 0 1
-	return_result
-    fi
 done
 
 for i in {1..50}; do
     ssh_connection_test tcp 1 2 1234
-    ret=$?
-    if [ $ret == 1 ]; then
-	qemu_stop 1
-	qemu_stop 2
-	server_stop 0
-	network_disconnect 0 1
-	return_result
-    fi
     ssh_connection_test tcp 2 1 1234
-    ret=$?
-    if [ $ret == 1 ]; then
-	qemu_stop 1
-	qemu_stop 2
-	server_stop 0
-	network_disconnect 0 1
-	return_result
-    fi
 done
 qemu_stop 1
 qemu_stop 2
