d80445472141c1c8fc65977f58be462a6b2d0ff0
Jerome Jutteau <jerome.jutteau@outscale.com>
2017-01-13 13:48:22 +0100

    bench: fix butterfly configuration
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- benchmarks/functions.sh
+++ benchmarks/functions.sh
@@ -42,7 +42,8 @@ function butterfly_start {
     local id=$1
     local ip=$2
     local port=$3
-    ssh_run $ip $port "echo dpdk-args=--no-shconf -c1 -n2 --socket-mem 64 > /tmp/butt-config.ini"
+    ssh_run $ip $port "echo [general] > /tmp/butt-config.ini"
+    ssh_run $ip $port "echo dpdk-args=--no-shconf -c1 -n2 --socket-mem 64 --huge-unlink >> /tmp/butt-config.ini"
     ssh_run $ip $port tmux new -d -s $(date +%Y-%m-%d-%H.%M.%S) \'butterflyd -c /tmp/butt-config.ini -l debug -i 43.0.0.$id -s /tmp \'
     sleep 5
     ssh_run $ip $port pgrep -f butterflyd
