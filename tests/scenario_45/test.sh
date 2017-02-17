6bfafcbd5f4c34e5860a2e728fb4b02b82499e4c
Jerome Jutteau <jerome.jutteau@outscale.com>
2017-02-17 09:32:49 +0100

    tests: rewrite of scenario_45
    Avoid code repetition for different scenario (trace, no trace, etc..)
    
    closes #243
    
    Signed-off-by: Jerome Jutteau <jerome.jutteau@outscale.com>

--- tests/scenario_45/test.sh
+++ tests/scenario_45/test.sh
@@ -6,266 +6,63 @@ source $BUTTERFLY_SRC_ROOT/tests/functions.sh
 
 network_connect 0 1
 
-# Case 1: no trace option, no filtering bypass
-server_start_options 0
-server_start_options 1
-nic_add 0 1 42 sg-1
-nic_add 1 2 42 sg-1
-qemu_start 1
-qemu_start 2
-sg_rule_add_icmp 0 sg-1
-sg_rule_add_icmp 1 sg-1
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-nic_add 0 4 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+# All combinations is too long (2^4 = 16 scenario)
+# for big in {\ +,-t+}{nic_add+,nic_add_bypass+}{nic_add+,nic_add_bypass+}{nic_add+,nic_add_bypass+} ; do
+# Remove some cases
+for big in \
+    +nic_add+nic_add+nic_add+ \
+    +nic_add+nic_add_bypass+nic_add+ \
+    +nic_add_bypass+nic_add_bypass+nic_add_bypass+ \
+    -t+nic_add+nic_add+nic_add+ \
+    -t+nic_add_bypass+nic_add+nic_add_bypass+ \
+    -t+nic_add_bypass+nic_add_bypass+nic_add_bypass+
+    do
 
-# Case 2: no trace option, filtering bypass
-server_start_options 0 
-server_start_options 1
-nic_add_bypass 0 1 42
-nic_add_bypass 1 2 42
-qemu_start 1
-qemu_start 2
-ssh_ping 1 2
-nic_add_bypass 0 3 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add_bypass 0 3 42
-nic_add_bypass 0 4 42
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    _server_start_options=$(echo $big | cut -d '+' -f 1)
+    _nic_add_1=$(echo $big | cut -d '+' -f 2)
+    _nic_add_2=$(echo $big | cut -d '+' -f 3)
+    _nic_add_3=$(echo $big | cut -d '+' -f 4)
 
-# Case 3: trace option, no filtering bypass
-server_start_options 0 -t
-server_start_options 1 -t
-nic_add 0 1 42 sg-1
-nic_add 1 2 42 sg-1
-qemu_start 1
-qemu_start 2
-sg_rule_add_icmp 0 sg-1
-sg_rule_add_icmp 1 sg-1
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-nic_add 0 4 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    # both servers start with or without trace option
+    server_start_options 0 $_server_start_options
+    server_start_options 1 $_server_start_options
+    nic_add 0 1 42 sg-1
+    nic_add 1 2 42 sg-1
+    qemu_start 1
+    qemu_start 2
+    sg_rule_add_icmp 0 sg-1
+    sg_rule_add_icmp 1 sg-1
+    ssh_ping 1 2
 
-# Case 4: trace option, filtering bypass
-server_start_options 0 -t
-server_start_options 1 -t
-nic_add_bypass 0 1 42
-nic_add_bypass 1 2 42
-qemu_start 1
-qemu_start 2
-ssh_ping 1 2
-nic_add_bypass 0 3 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add_bypass 0 3 42
-nic_add_bypass 0 4 42
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    # nic added with or without bypass
+    $_nic_add_1 0 3 42 sg-1
 
-# Case 5.1 no trace option, mixed filtering bypass scenario
-server_start_options 0
-server_start_options 1
-nic_add 0 1 42 sg-1
-nic_add_bypass 1 2 42
-qemu_start 1
-qemu_start 2
-sg_rule_add_icmp 0 sg-1
-sg_rule_add_icmp 1 sg-1
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-nic_add 0 4 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    qemu_start 3
+    ssh_ping 1 2
+    ssh_ping 1 3
+    ssh_ping 3 2
+    qemu_stop 3
+    nic_del 0 3
+    ssh_ping 1 2
 
-# Case 5.2 no trace option, mixed filtering bypass scenario
-server_start_options 0
-server_start_options 1
-nic_add 0 1 42 sg-1
-nic_add 1 2 42 sg-1
-qemu_start 1
-qemu_start 2
-sg_rule_add_icmp 0 sg-1
-sg_rule_add_icmp 1 sg-1
-ssh_ping 1 2
-nic_add_bypass 0 3 42
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add_bypass 0 3 42
-nic_add_bypass 0 4 42
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    # nic added with or without bypass
+    $_nic_add_2 0 3 42 sg-1
 
-# Case 6.1 trace option, mixed filtering bypass scenario
-server_start_options 0 -t
-server_start_options 1 -t
-nic_add 0 1 42 sg-1
-nic_add_bypass 1 2 42
-qemu_start 1
-qemu_start 2
-sg_rule_add_icmp 0 sg-1
-sg_rule_add_icmp 1 sg-1
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-ssh_ping 1 2
-nic_add 0 3 42 sg-1
-nic_add 0 4 42 sg-1
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    # nic added with or without bypass
+    $_nic_add_3 0 4 42 sg-1
 
-# Case 6.2 trace option, mixed filtering bypass scenario
-server_start_options 0 -t
-server_start_options 1 -t
-nic_add 0 1 42 sg-1
-nic_add 1 2 42 sg-1
-qemu_start 1
-qemu_start 2
-sg_rule_add_icmp 0 sg-1
-sg_rule_add_icmp 1 sg-1
-ssh_ping 1 2
-nic_add_bypass 0 3 42
-qemu_start 3
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-ssh_ping 1 2
-nic_del 0 3
-ssh_ping 1 2
-nic_add_bypass 0 3 42
-nic_add_bypass 0 4 42
-qemu_start 3
-ssh_ping 1 2
-ssh_ping 1 3
-ssh_ping 3 2
-qemu_stop 3
-nic_del 0 3
-nic_del 0 4
-ssh_ping 1 2
-qemu_stop 1
-qemu_stop 2
-server_stop 0
-server_stop 1
+    qemu_start 3
+    ssh_ping 1 2
+    ssh_ping 1 3
+    ssh_ping 3 2
+    qemu_stop 3
+    nic_del 0 3
+    nic_del 0 4
+    ssh_ping 1 2
+    qemu_stop 1
+    qemu_stop 2
+    server_stop 0
+    server_stop 1
 
+done
 network_disconnect 0 1
-return_result
-
