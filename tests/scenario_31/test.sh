5e3f8dc5e3be57d98862922701d8d09286575e8e
Matthias Gatto <matthias.gatto@outscale.com>
2017-03-07 10:44:40 +0100

    tests: start qemu asynchronously in scenarios
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- tests/scenario_31/test.sh
+++ tests/scenario_31/test.sh
@@ -9,8 +9,7 @@ server_start 0
 nic_add 0 1 42 sg-1
 nic_add 0 2 42
 sg_rule_add_port_open udp 0 8000 sg-1
-qemu_start 1
-qemu_start 2
+qemus_start 1 2
 
 sg_rule_add_port_open udp 0 9000 sg-2
 ssh_connection_test udp 2 1 8000
@@ -29,8 +28,7 @@ remove_sg_from_nic 0 1
 ssh_no_connection_test udp 2 1 8000
 ssh_no_connection_test udp 2 1 9000
 
-qemu_stop 1
-qemu_stop 2
+qemus_stop 1 2
 server_stop 0
 network_disconnect 0 1
 return_result
