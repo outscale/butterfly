68f77954ffd666ed97622e2dd7508fe08f9c9385
Matthias Gatto <matthias.gatto@outscale.com>
2018-04-13 09:19:55 +0000

    tests: really test tap
    before this tests, ping and iperf return was printing to stdout,
    but wasn't tested
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- tests/other/scenario_02/test.sh
+++ tests/other/scenario_02/test.sh
@@ -17,17 +17,13 @@ tap_add 0 3 42 sg-1
 sg_rule_add_all_open 0 sg-1
 sg_rule_add_all_open 1 sg-1
 
-sudo ip netns exec ns1 ping 42.0.0.2 -c 5
-sudo ip netns exec ns2 ping 42.0.0.1 -c 5
-sudo ip netns exec ns1 ping 42.0.0.3 -c 5
+tap_ping 1 2
+tap_ping 2 1
+tap_ping 1 3
 
-sudo ip netns exec ns1 iperf3 -s &
-sleep 1
-sudo ip netns exec ns2 iperf3 -c 42.0.0.1 -t 5
-
-sudo ip netns exec ns1 iperf3 -s &
-sleep 1
-sudo ip netns exec ns3 iperf3 -c 42.0.0.1 -t 5
+tap_iperf3_tcp 1 2
+tap_iperf3_tcp 2 1
+tap_iperf3_tcp 1 3
 
 network_disconnect 0 1
 server_stop 0
