6d275545dcbfb2a1dc8d9fb3c657279fb620fe6e
Matthias Gatto <matthias.gatto@outscale.com>
2019-03-11 14:51:42 +0100

    3rdparty: build packetgraph using a script
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ scripts/build_packetgraph.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo current dir $(pwd)
+
+echo packetgraph source dir: $1
+PG_SOURCE_DIR=$1
+
+echo packetgraph install dir: $2
+PG_INSTALL_DIR=$2
+
+echo dpdk dir: $RTE_SDK
+
+$PG_SOURCE_DIR/autogen.sh $PG_SOURCE_DIR
+
+CFLAGS="-march=ivybridge -DPG_BRICK_NO_ATOMIC_COUNT -O2 -g" $PG_SOURCE_DIR/configure_clang --prefix="$PG_INSTALL_DIR" --with-examples
+
